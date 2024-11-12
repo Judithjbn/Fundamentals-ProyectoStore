a7da138178da30bd3d2ab001824f1354c067af35
Judith <50967294+Judithjbn@users.noreply.github.com>
2024-11-12 18:37:54 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import { products } from '/products.js';
+// const productsList = document.querySelector(".products-list");
+
+const showModal = () => {
+  const modal = document.querySelector(".filter-modal");
+  
+}
---
+++ js/products.js
@@ -0,0 +1,114 @@
+const productos = [
+  {
+    id: 1,
+    nombre: 'New Balance 530 Mujer',
+    marca: 'New Balance',
+    color: ['Blanco', 'Marron', 'Beige'],
+    precio: 120,
+    imagen: '/assets/new-balance-530.webp'
+  },
+  {
+    id: 2,
+    nombre: 'adidas Originals Campus 00s',
+    marca: 'Adidas',
+    color: ['Gris', 'Azul', 'Negro', 'Verde'],
+    precio: 120,
+    imagen: '/assets/adidas-Originals-campus.webp'
+  },
+  {
+    id: 3,
+    nombre: 'New Balance 327',
+    marca: 'New Balance',
+    color: ['Azul Marino', 'Gris', 'Verde'],
+    precio: 130,
+    imagen: '/assets/new-balance-327.webp'
+  },
+  {
+    id: 4,
+    nombre: 'Vans Old Skool',
+    marca: 'Vans',
+    color: ['Negro'],
+    precio: 80,
+    imagen: '/assets/vans-old-skool.webp'
+  },
+  {
+    id: 5,
+    nombre: 'Puma E Cali',
+    marca: 'Puma',
+    color: ['Blanco', 'Rosa'],
+    precio: 60,
+    imagen: '/assets/puma-cali.webp'
+  },
+  {
+    id: 6,
+    nombre: 'Reebok Classic Leather',
+    marca: 'Reebok',
+    color: ['Blanco'],
+    precio: 90,
+    imagen: '/assets/reebok-classic-leather.webp'
+  },
+  {
+    id: 7,
+    nombre: 'Converse Chuck Taylor All Star',
+    marca: 'Converse',
+    color: ['Blanco', 'Negro'],
+    precio: 8,
+    imagen: '/assets/converse-chuck-taylor-all-star.webp'
+  },
+  {
+    id: 8,
+    nombre: 'Fila Disruptor II',
+    marca: 'Fila',
+    color: ['Blanco', 'Rosa', 'Azul', 'Verde'],
+    precio: 60,
+    imagen: '/assets/fila-disruptor-ii.webp'
+  },
+  {
+    id: 9,
+    nombre: 'adidas Originals Superstar',
+    marca: 'Adidas',
+    color: ['Negro'],
+    precio: 140,
+    imagen: '/assets/adidas-originals-superstar.webp'
+  },
+  {
+    id: 10,
+    nombre: 'Nike Blazer Mid',
+    marca: 'Nike',
+    color: ['Rojo'],
+    precio: 105,
+    imagen: '/assets/nike-blazer-mid-77.webp'
+  },
+  {
+    id: 11,
+    nombre: 'Jordan Air Jordan 1 Mid',
+    marca: 'Jordan',
+    color: ['Azul', 'Negro', 'Rojo'],
+    precio: 125,
+    imagen: '/assets/jordan-air-jordan-1-mid.webp'
+  },
+  {
+    id: 12,
+    nombre: 'Asics Gel-Quantum 360',
+    marca: 'Asics',
+    color: ['Negro', 'Naranja'],
+    precio: 140,
+    imagen: '/assets/asics-gel-quantum-360.webp'
+  },
+  {
+    id: 13,
+    nombre: 'Saucony Jazz Original',
+    marca: 'Saucony',
+    color: ['Blanco', 'Gris', 'Azul', 'Verde', 'Amarillo'],
+    precio: 90,
+    imagen: '/assets/saucony-jazz-original.webp'
+  },
+  {
+    id: 14,
+    nombre: 'Timberland 6 Inch Premium',
+    marca: 'Timberland',
+    color: ['Marrón', 'Negro'],
+    precio: 220,
+    imagen: '/assets/timberland-6-inch-premium.webp'
+  }
+]
