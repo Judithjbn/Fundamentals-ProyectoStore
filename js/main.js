b50011e7bbf9873f39dcdc5e66eb0071672ca074
Judith <jbarrilnavarro@gmail.com>
2024-11-13 10:44:59 +0100

    corrección evento mostrar/ocultar modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-import { products } from '/products.js';
+//import { products } from '/products.js';
 // const productsList = document.querySelector(".products-list");
 
-const showModal = () => {
-  const modal = document.querySelector(".filter-modal");
-  
-}
+document.getElementById('filter-btn').addEventListener('click', () => {
+  const modal = document.querySelector('.filter-modal');
+  modal.style.display = (modal.style.display === "flex") ? "none" : "flex";
+});
+
+document.getElementById('close-filter').addEventListener('click', () => {
+  const modal = document.querySelector('.filter-modal');
+  modal.style.display = "none";
+});
